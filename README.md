7e9601fa4ffbf8f7c54044167bc803e083a0d81a
DidDrog11 <dsimons1987@gmail.com>
2021-09-27 11:10:48 +0100

    upgrade draft

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# phd_project
+# PhD Thesis
+
+I intend to write my thesis as a markdown that can is reproducible and can be modified from anywhere. This is the repository for this.
  
--- scripts/gantt_chart.R
+++ scripts/gantt_chart.R
@@ -12,10 +12,12 @@ gantt_chart <- ganttrify(project = gantt_activities,
           size_text_relative = 1.2, 
           axis_text_align = "left",
           mark_quarters = TRUE,
-          font_family = "Roboto Condensed") +
+          font_family = "Roboto Condensed",
+          colour_palette = wesanderson::wes_palette("Zissou1", n = 8, type = "continuous")) +
   theme(axis.text.y = element_text(hjust = 0),
         axis.text.x = element_text(color = c("gray30", 0, 0))) +
   labs(title = "PhD project timeline, 2020-2023",
-       caption = "C = completed, O = PhD output, S = PhD submission deadline")
+       caption = "C = completed, F = Failed to achieve, O = PhD output, S = PhD submission deadline")
 
-ggsave(filename = here("output", "gantt.png"), dpi = 300, plot = gantt_chart)
+ggsave(filename = here("output", "gantt.png"), dpi = 300, plot = gantt_chart,
+       width = 10, height = 11)
