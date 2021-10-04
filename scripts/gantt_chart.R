a027239dfcb0d0c80ab04bb711675cf6005182c8
DidDrog11 <dsimons1987@gmail.com>
2021-10-04 13:49:38 +0100

    update after supervisor comments

--- scripts/gantt_chart.R
+++ scripts/gantt_chart.R
@@ -1,6 +1,4 @@
 source(here::here("scripts", "library.R"))
-remotes::install_github("giocomai/ganttrify")
-library("ganttrify")
 
 gantt_activities <- readxl::read_excel(here("data", "project_timelines.xlsx"), sheet = 1)
 gantt_spots <- readxl::read_excel(here("data", "project_timelines.xlsx"), sheet = 2)
@@ -17,7 +15,7 @@ gantt_chart <- ganttrify(project = gantt_activities,
   theme(axis.text.y = element_text(hjust = 0),
         axis.text.x = element_text(color = c("gray30", 0, 0))) +
   labs(title = "PhD project timeline, 2020-2023",
-       caption = "C = completed, F = Failed to achieve, O = PhD output, S = PhD submission deadline")
+       caption = "C = Completed, F = Failed to achieve, O = PhD output, S = PhD submission deadline")
 
 ggsave(filename = here("output", "gantt.png"), dpi = 300, plot = gantt_chart,
        width = 10, height = 11)
--- scripts/library.R
+++ scripts/library.R
@@ -1,9 +1,11 @@
-devtools::install_github("DidDrog11/DavidsPack")
+# devtools::install_github("DidDrog11/DavidsPack")
+remotes::install_github("giocomai/ganttrify")
 
 if (!require("pacman")) install.packages("pacman")
 pkgs = c(
   "here",
-  "tidyverse"
+  "tidyverse",
+  "ganttrify"
 )
 
 pacman::p_load(pkgs, character.only = T)
